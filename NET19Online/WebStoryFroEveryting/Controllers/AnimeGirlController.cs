954e1d1eb8bdd22e17251875458bc84bd803a1e5
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2025-02-05 21:23:26 +0300

    Task-1 Add overlord girl

--- NET19Online/WebStoryFroEveryting/Controllers/AnimeGirlController.cs
+++ NET19Online/WebStoryFroEveryting/Controllers/AnimeGirlController.cs
@@ -14,15 +14,20 @@ namespace WebStoryFroEveryting.Controllers
                     Name = "Юки",
                     Src  = "https://e7.pngegg.com/pngimages/695/533/png-clipart-brown-haired-female-anime-character-wearing-black-uniform-yuki-cross-kaname-kuran-zero-kiryu-vampire-knight-vampire-black-hair-manga.png"
                 },
-                 new IdolViewModel
+                new IdolViewModel
                 {
                     Name = "Кирито",
                     Src  = "https://c4.wallpaperflare.com/wallpaper/48/394/450/swordartonline-anime-girls-kirito-sword-art-online-asuna-sword-art-online-hd-wallpaper-preview.jpg"
                 },
-                  new IdolViewModel
+                 new IdolViewModel
                 {
                     Name = "Юки 3",
                     Src  = "https://e7.pngegg.com/pngimages/695/533/png-clipart-brown-haired-female-anime-character-wearing-black-uniform-yuki-cross-kaname-kuran-zero-kiryu-vampire-knight-vampire-black-hair-manga.png"
+                },
+                new IdolViewModel
+                {
+                    Name = "Overlord",
+                    Src  = "https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/03/CBR-Featured-Images-Albedo-Overlord.jpg"
                 }
             };
 
