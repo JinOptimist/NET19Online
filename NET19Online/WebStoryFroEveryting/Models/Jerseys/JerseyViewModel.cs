04556f0bb8625820156d14d54f8e6bae1da577c3
GregSklyanny <gregsklyanny@gmail.com>
2025-02-06 21:16:46 +0300

    Add Jerseys Store

---
+++ NET19Online/WebStoryFroEveryting/Models/Jerseys/JerseyViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebStoryFroEveryting.Models.Jerseys
+{
+    public class JerseyViewModel
+    {
+        public int Id { get; set; }
+        public string Club { get; set; }
+        public int Number {  get; set; }
+        public string AthleteName { get; set; }
+        public string Img { get; set; }
+    }
+}
