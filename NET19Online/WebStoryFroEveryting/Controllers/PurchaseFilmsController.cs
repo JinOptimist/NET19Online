ff04f1c2c28b7dbaecde5f26c1ce7d08e26c6ed3
Artur <arturdemirchyan21@gmail.com>
2025-02-08 23:32:49 +0300

    PurchaseFilms

---
+++ NET19Online/WebStoryFroEveryting/Controllers/PurchaseFilmsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStoryFroEveryting.Controllers
+{
+    public class PurchaseFilmsController : Controller
+    {
+        public IActionResult CreatePurchaseFilms()
+        {
+            return View();
+        }
+
+   
+    }
+}
