48873ba64d0da21400eeab51e0a9077139e3b702
Andrey <andreymoiseenkohub@gmail.com>
2025-02-09 05:31:57 +0300

    add Details page and Details Controller

--- NET19Online/WebStoryFroEveryting/Controllers/LessonsController.cs
+++ NET19Online/WebStoryFroEveryting/Controllers/LessonsController.cs
@@ -5,37 +5,73 @@ namespace WebStoryFroEveryting.Controllers;
 
 public class LessonsController : Controller
 {
+    // Вынести 
+    List<LessonViewModel> lessons = new List<LessonViewModel>
+    {
+        new LessonViewModel()
+        {
+            Id = 1,
+            Title = "Переменные и типы данных",
+            Source = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/44sBcVvn-eI?si=nB0zvkicLKgUHMzp\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>",
+            Preview = "https://cdni.iconscout.com/illustration/premium/thumb/404-illustration-download-in-svg-png-gif-file-formats--error-not-found-page-result-webpage-pack-user-interface-illustrations-5974976.png?f=webp"
+        },
+        new LessonViewModel()
+        {
+            Id = 2,
+            Title = "Условные операторы",
+            Source = "https://www.youtube.com/watch?v=JD3Ois6i298&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=6",
+            Preview = "https://cdni.iconscout.com/illustration/premium/thumb/404-illustration-download-in-svg-png-gif-file-formats--error-not-found-page-result-webpage-pack-user-interface-illustrations-5974976.png?f=webp"
+        },
+        new LessonViewModel()
+        {
+            Id = 3,
+            Title = "Циклы",
+            Source = "https://www.youtube.com/watch?v=JD3Ois6i298&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=6",
+            Preview = "https://cdni.iconscout.com/illustration/premium/thumb/404-illustration-download-in-svg-png-gif-file-formats--error-not-found-page-result-webpage-pack-user-interface-illustrations-5974976.png?f=webp"
+        },
+        new LessonViewModel()
+        {
+            Id = 4,
+            Title = "Методы",
+            Source = "https://www.youtube.com/watch?v=JD3Ois6i298&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=6",
+            Preview = "https://cdni.iconscout.com/illustration/premium/thumb/404-illustration-download-in-svg-png-gif-file-formats--error-not-found-page-result-webpage-pack-user-interface-illustrations-5974976.png?f=webp"
+        },
+        new LessonViewModel()
+        {
+            Id = 5,
+            Title = "Методы",
+            Source = "https://www.youtube.com/watch?v=JD3Ois6i298&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=6",
+            Preview = "https://cdni.iconscout.com/illustration/premium/thumb/404-illustration-download-in-svg-png-gif-file-formats--error-not-found-page-result-webpage-pack-user-interface-illustrations-5974976.png?f=webp"
+        },
+        new LessonViewModel()
+        {
+            Id = 6,
+            Title = "Методы",
+            Source = "https://www.youtube.com/watch?v=JD3Ois6i298&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=6",
+            Preview = "https://cdni.iconscout.com/illustration/premium/thumb/404-illustration-download-in-svg-png-gif-file-formats--error-not-found-page-result-webpage-pack-user-interface-illustrations-5974976.png?f=webp"
+        },
+        new LessonViewModel()
+        {
+            Id = 7,
+            Title = "Методы",
+            Source = "https://www.youtube.com/watch?v=JD3Ois6i298&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=6",
+            Preview = "https://cdni.iconscout.com/illustration/premium/thumb/404-illustration-download-in-svg-png-gif-file-formats--error-not-found-page-result-webpage-pack-user-interface-illustrations-5974976.png?f=webp"
+        },
+    };
     public IActionResult Index()
     {
-        var lessons = new List<LessonViewModel>
-        {
-            new LessonViewModel()
-            {
-                Id = 1,
-                Title = "Переменные и типы данных",
-                Source = "https://www.youtube.com/watch?v=JD3Ois6i298&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=6",
-                Preview = "https://cdni.iconscout.com/illustration/premium/thumb/404-illustration-download-in-svg-png-gif-file-formats--error-not-found-page-result-webpage-pack-user-interface-illustrations-5974976.png?f=webp"
-            },
-            new LessonViewModel()
-            {
-                Id = 2,
-                Title = "Условные операторы",
-                Source = "https://www.youtube.com/watch?v=JD3Ois6i298&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=6",
-                Preview = "https://cdni.iconscout.com/illustration/premium/thumb/404-illustration-download-in-svg-png-gif-file-formats--error-not-found-page-result-webpage-pack-user-interface-illustrations-5974976.png?f=webp"
-            },
-            new LessonViewModel()
-            {
-                Id = 3,
-                Title = "Циклы",
-                Source = "https://www.youtube.com/watch?v=JD3Ois6i298&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=6",
-                Preview = "https://cdni.iconscout.com/illustration/premium/thumb/404-illustration-download-in-svg-png-gif-file-formats--error-not-found-page-result-webpage-pack-user-interface-illustrations-5974976.png?f=webp"
-            },
-        };
+
         return View(lessons);
     }
 
     public IActionResult Details(int id)
     {
-        return View();
+        foreach (var item in lessons)
+        {
+            if (item.Id == id)
+                return View(item);
+        }
+
+        throw new ArgumentException("Index not found");
     }
 }
