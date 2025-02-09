0dfa63554caeb896d27075a6927ddc9dd1b02f84
Andrey <andreymoiseenkohub@gmail.com>
2025-02-10 02:53:08 +0300

    add Details styles

--- NET19Online/WebStoryFroEveryting/Controllers/LessonsController.cs
+++ NET19Online/WebStoryFroEveryting/Controllers/LessonsController.cs
@@ -19,42 +19,28 @@ public class LessonsController : Controller
         {
             Id = 2,
             Title = "Условные операторы",
-            Source = "https://www.youtube.com/watch?v=JD3Ois6i298&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=6",
+            Source = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/44sBcVvn-eI?si=nB0zvkicLKgUHMzp\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>",
             Preview = "https://cdni.iconscout.com/illustration/premium/thumb/404-illustration-download-in-svg-png-gif-file-formats--error-not-found-page-result-webpage-pack-user-interface-illustrations-5974976.png?f=webp"
         },
         new LessonViewModel()
         {
             Id = 3,
             Title = "Циклы",
-            Source = "https://www.youtube.com/watch?v=JD3Ois6i298&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=6",
+            Source = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/44sBcVvn-eI?si=nB0zvkicLKgUHMzp\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>",
             Preview = "https://cdni.iconscout.com/illustration/premium/thumb/404-illustration-download-in-svg-png-gif-file-formats--error-not-found-page-result-webpage-pack-user-interface-illustrations-5974976.png?f=webp"
         },
         new LessonViewModel()
         {
             Id = 4,
             Title = "Методы",
-            Source = "https://www.youtube.com/watch?v=JD3Ois6i298&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=6",
+            Source = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/44sBcVvn-eI?si=nB0zvkicLKgUHMzp\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>",
             Preview = "https://cdni.iconscout.com/illustration/premium/thumb/404-illustration-download-in-svg-png-gif-file-formats--error-not-found-page-result-webpage-pack-user-interface-illustrations-5974976.png?f=webp"
         },
         new LessonViewModel()
         {
             Id = 5,
-            Title = "Методы",
-            Source = "https://www.youtube.com/watch?v=JD3Ois6i298&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=6",
-            Preview = "https://cdni.iconscout.com/illustration/premium/thumb/404-illustration-download-in-svg-png-gif-file-formats--error-not-found-page-result-webpage-pack-user-interface-illustrations-5974976.png?f=webp"
-        },
-        new LessonViewModel()
-        {
-            Id = 6,
-            Title = "Методы",
-            Source = "https://www.youtube.com/watch?v=JD3Ois6i298&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=6",
-            Preview = "https://cdni.iconscout.com/illustration/premium/thumb/404-illustration-download-in-svg-png-gif-file-formats--error-not-found-page-result-webpage-pack-user-interface-illustrations-5974976.png?f=webp"
-        },
-        new LessonViewModel()
-        {
-            Id = 7,
-            Title = "Методы",
-            Source = "https://www.youtube.com/watch?v=JD3Ois6i298&list=PLQOaTSbfxUtD6kMmAYc8Fooqya3pjLs1N&index=6",
+            Title = "Классы",
+            Source = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/44sBcVvn-eI?si=nB0zvkicLKgUHMzp\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>",
             Preview = "https://cdni.iconscout.com/illustration/premium/thumb/404-illustration-download-in-svg-png-gif-file-formats--error-not-found-page-result-webpage-pack-user-interface-illustrations-5974976.png?f=webp"
         },
     };
