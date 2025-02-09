888badfe40cdda7fd7efd993d2e36464255d1b3c
Artur <arturdemirchyan21@gmail.com>
2025-02-09 17:09:00 +0300

    Films

--- NET19Online/WebStoryFroEveryting/Controllers/PurchaseFilmsController.cs
+++ NET19Online/WebStoryFroEveryting/Controllers/PurchaseFilmsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebStoryFroEveryting.Models.Films;
 
 namespace WebStoryFroEveryting.Controllers
 {
@@ -7,7 +8,25 @@ namespace WebStoryFroEveryting.Controllers
     {
         public IActionResult CreatePurchaseFilms()
         {
-            return View();
+            var viewModels = new List<FilmsViewModel>
+            {
+               new FilmsViewModel
+               {
+                    Name = "Нация Z 1",
+                    Src="https://www.film.ru/sites/default/files/styles/epsa_260x400/public/movies/posters/4677076-1076094.jpg"
+               },
+               new FilmsViewModel
+               {
+                    Name = "Нация Z 2",
+                    Src="https://www.film.ru/sites/default/files/styles/epsa_260x400/public/movies/posters/4677076-1076094.jpg"
+               },
+               new FilmsViewModel
+               {
+                    Name = "Нация Z 3",
+                    Src="https://www.film.ru/sites/default/files/styles/epsa_260x400/public/movies/posters/4677076-1076094.jpg"
+               }
+            };
+            return View(viewModels);
         }
 
    
---
+++ NET19Online/WebStoryFroEveryting/Models/Films/FilmsViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebStoryFroEveryting.Models.Films
+{
+    public class FilmsViewModel
+    {
+        public string Name { get; set; }
+        public string Src { get; set; }
+    }
+}
