29b1a5e4ce2cfc2d457b9c530c35359e5373fc02
SashaD <daniav1987@dmail.com>
2025-02-09 21:15:28 +0300

    Task-11 add cod for underwater hunter page

---
+++ NET19Online/UnderwaterHunterProject/Controllers/UnderwaterHunterController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UnderwaterHunterProject.Models.UnderwaterHuntersWorld;
+
+namespace UnderwaterHunterProject.Controllers
+{
+    public class UnderwaterHunterController : Controller
+    {
+        public IActionResult CreatePageUnderwaterHunter()
+        {
+            var hunters = new List<TheBestUnderwaterHunters>
+            {
+                new TheBestUnderwaterHunters
+                {
+                    NameHunter="Pedro Carbonell",
+                    Nationality = "Spanish",
+                    maxHuntingDepth= 40,
+                    Src = "https://avatars.mds.yandex.net/i?id=d37489e48b123dee24adcce63e1304a5_l-5312143-images-thumbs&n=13"
+                },
+                new TheBestUnderwaterHunters
+                {
+                    NameHunter="Gabriele Delbene",
+                    Nationality = "Italian",
+                    maxHuntingDepth= 62,
+                    Src = "https://www.batiskaf.ru/media/wysiwyg/wordpress/2014/12/DSCN2440.jpg"
+                }
+            };
+            return View(hunters);
+        }
+    }
+}
---
+++ NET19Online/UnderwaterHunterProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UnderwaterHunterProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
