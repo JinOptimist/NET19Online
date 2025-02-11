bec033fe6486865747fd7d0f393b38698c64f545
Nibicu <Wams9119@Gmail.com>
2025-02-12 02:21:18 +0700

    Task-30 Add my repository for Notebooks

---
+++ NET19Online/StoreData/Repostiroties/NotebookRepository.cs
@@ -0,0 +1,31 @@
+﻿using StoreData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreData.Repostiroties
+{
+    public class NotebookRepository
+    {
+        private static List<NotebookData> FakeDB = new();
+
+        public List<NotebookData> GetNotebooks() 
+        {
+            return FakeDB; 
+        }
+
+        public void AddNotebook(NotebookData notebook)
+        {
+            notebook.Id = FakeDB.Count > 0 ? FakeDB.Max(x => x.Id) + 1:  1;
+            FakeDB.Add(notebook);
+        }
+
+        public void Remove(int id) 
+        {
+            FakeDB = FakeDB.Where(x => x.Id != id).ToList();
+        }
+
+    }
+}
