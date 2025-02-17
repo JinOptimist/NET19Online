6296ca6d549ac77850a1e73229f333fdfafd76ad
SashaD <daniav1987@dmail.com>
2025-02-17 09:41:55 +0300

    Task-53 completed (create BD for underwaterHunter)

--- NET19Online/StoreData/StoreDbContext.cs
+++ NET19Online/StoreData/StoreDbContext.cs
@@ -9,8 +9,8 @@ namespace StoreData
 
         public DbSet<IdolData> Idols { get; set; }
         public DbSet<MagicItemData> MagicItems { get; set; }
-
-
+        public DbSet<UnderwaterHunterData> UnderwaterHunters { get; set; }
+        
         public StoreDbContext() { }
         public StoreDbContext(DbContextOptions option) : base(option) { }
 
