d4938d2482115214560a93779a1daeac192c2557
GregSklyanny <gregsklyanny@gmail.com>
2025-02-27 23:53:20 +0300

    CleanCode

--- NET19Online/WebStoryFroEveryting/Models/CustomValidationAttribute/IsPositiveNumber.cs
+++ NET19Online/WebStoryFroEveryting/Models/CustomValidationAttribute/IsPositiveNumber.cs
@@ -6,12 +6,8 @@ namespace WebStoryFroEveryting.Models.CustomValidationAttribute
     {
         protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
         {
-            var number = (T) value!;
-            var zero = T.Zero;
-            
-            
-            if (!T.IsPositive(number) ) return new ValidationResult("Значение меньше нуля!");
-
+            var number = (T)value!;
+            if (!T.IsPositive(number)) return new ValidationResult("Значение меньше нуля!");
             return ValidationResult.Success;
         }
     }
