cd7ecc4024c5a3b97db81be9a3a10e7b017977ff
GregSklyanny <gregsklyanny@gmail.com>
2025-02-27 23:50:33 +0300

    Add Permissions And Validators

--- NET19Online/WebStoryFroEveryting/Models/Jerseys/CreateJerseyViewModel.cs
+++ NET19Online/WebStoryFroEveryting/Models/Jerseys/CreateJerseyViewModel.cs
@@ -1,12 +1,22 @@
-﻿namespace WebStoryFroEveryting.Models.Jerseys
+﻿using System.ComponentModel.DataAnnotations;
+using WebStoryFroEveryting.Models.CustomValidationAttribute;
+namespace WebStoryFroEveryting.Models.Jerseys
 {
     public class CreateJerseyViewModel
     {
+        [RangeLength(3)]
         public string Club { get; set; }
+        [IsPositiveNumber<int>]
         public int Number { get; set; }
+        [Required]
+        [RangeLength(1)]
         public string AthleteName { get; set; }
+        [Required]
         public string Img { get; set; }
+        [IsPositiveNumber<int>]
         public int InStock { get; set; }
+        [Required]
+        [IsPositiveNumber<decimal>]
         public decimal Price { get; set; }
     }
 }
