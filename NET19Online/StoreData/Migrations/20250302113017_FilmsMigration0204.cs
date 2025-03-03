7b81d6c73f3edb732d4acfd4aafedad60a8b7fad
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2025-03-03 19:57:58 +0300

    Fix migration for film

--- NET19Online/StoreData/Migrations/20250302113017_FilmsMigration0204.cs
+++ NET19Online/StoreData/Migrations/20250302113017_FilmsMigration0204.cs
@@ -31,76 +31,11 @@ namespace StoreData.Migrations.StoreDb
                         principalColumn: "Id");
                 });
 
-            migrationBuilder.CreateTable(
-                name: "MagicItemDataMagicItemTagData",
-                columns: table => new
-                {
-                    MagicItemsId = table.Column<int>(type: "int", nullable: false),
-                    TagsId = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_MagicItemDataMagicItemTagData", x => new { x.MagicItemsId, x.TagsId });
-                    table.ForeignKey(
-                        name: "FK_MagicItemDataMagicItemTagData_MagicItemTags_TagsId",
-                        column: x => x.TagsId,
-                        principalTable: "MagicItemTags",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_MagicItemDataMagicItemTagData_MagicItems_MagicItemsId",
-                        column: x => x.MagicItemsId,
-                        principalTable: "MagicItems",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "PlayerDataPlayerTagData",
-                columns: table => new
-                {
-                    PlayersId = table.Column<int>(type: "int", nullable: false),
-                    TagsId = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_PlayerDataPlayerTagData", x => new { x.PlayersId, x.TagsId });
-                    table.ForeignKey(
-                        name: "FK_PlayerDataPlayerTagData_FootballPlayers_PlayersId",
-                        column: x => x.PlayersId,
-                        principalTable: "FootballPlayers",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_PlayerDataPlayerTagData_PlayerTags_TagsId",
-                        column: x => x.TagsId,
-                        principalTable: "PlayerTags",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
 
             migrationBuilder.CreateIndex(
                 name: "IX_FilmCommentDatas_FilmId",
                 table: "FilmCommentDatas",
                 column: "FilmId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_MagicItemDataMagicItemTagData_TagsId",
-                table: "MagicItemDataMagicItemTagData",
-                column: "TagsId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_PlayerDataPlayerTagData_TagsId",
-                table: "PlayerDataPlayerTagData",
-                column: "TagsId");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_DescriptionFilms_Films_Id",
-                table: "DescriptionFilms",
-                column: "Id",
-                principalTable: "Films",
-                principalColumn: "Id",
-                onDelete: ReferentialAction.Cascade);
         }
 
         /// <inheritdoc />
