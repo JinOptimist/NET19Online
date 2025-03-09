20946f9cb8e57538635d3fe4dfaaa88a33564ae8
Mark Shcerbakov <markshcerbakov@gmail.com>
2025-03-09 22:54:20 +0300

    Task-106 complete

---
+++ NET19Online/WebStoryFroEveryting/wwwroot/js/Player/description.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $(".tags .tag").click(function () {
+        $(this).addClass("active");
+    });
+
+    $(".tags .tag").click(function () {
+        $(this).closest(".player-container").addClass("tag-border");
+    });
+});
