b81c346545c2fea8a656bd8b9caee2fefe1a81c9
Artur <arturdemirchyan21@gmail.com>
2025-03-11 20:32:33 +0300

    Point

--- NET19Online/WebStoryFroEveryting/wwwroot/js/Films/indexFilm.js
+++ NET19Online/WebStoryFroEveryting/wwwroot/js/Films/indexFilm.js
@@ -1,1 +1,18 @@
-﻿
+﻿$(document).ready(function () {
+    $('.block-elements .name').click(function () {
+
+        $('.block-elements').removeClass('active')
+        $(this).addClass('active');
+        $(this).addClass('to-remove');
+
+        $('.soft-remove').click(function () {
+            const ITEM = $(this).closest('.block-elements');
+            ITEM.remove();
+
+        });
+
+     });
+
+
+
+});
