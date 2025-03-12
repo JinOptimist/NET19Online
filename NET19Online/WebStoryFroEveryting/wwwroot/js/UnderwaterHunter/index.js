2bc1333e22ec180b8bfca7d400f3d6ac69d462d3
SashaD <daniav1987@dmail.com>
2025-03-12 18:47:56 +0300

    Task -117, add ajax code

--- NET19Online/WebStoryFroEveryting/wwwroot/js/UnderwaterHunter/index.js
+++ NET19Online/WebStoryFroEveryting/wwwroot/js/UnderwaterHunter/index.js
@@ -2,6 +2,38 @@
     $('.hide-image').click(function () {
         $(this).closest('.hunters').remove();
     });
+
+    $(".removeIcon").click(function () {
+        const hunter = (this).closest(".hunters");
+        const id = $(hunter).attr('data-id');
+        hunter.remove();
+        $.get("/api/underwaterHunter/remove?id=" + id);
+    });
+
+    $(".creatHunter").click(function () {
+        const name = $(".new-name").val();
+        const nationality = $(".new-nationality").val();
+        const maxDepth = $(".new-maxDepth").val();
+        const image = $(".new-image").val();
+        const hunter = {
+            name: name,
+            nationality: nationality,
+            maxDepth: maxDepth,
+            image: image
+        };
+        $.get("/api/underwaterHunter/create?name=" + name
+            + "&nationality=" + nationality + "&maxDepth=" + maxDepth + "&image=" + image)
+            .then(function (id) {
+                const clone = $(".newHunters.hunters").clone();
+                clone.removeClass("sample");
+                clone.attr("data-id", id);
+                clone.find(".newHunters.image img").attr("src", image);
+                clone.find(".newHunters.name").text(name);
+                clone.insertBefore($(".newHunters"));                
+            });
+    });
+
 });
+
 
 
