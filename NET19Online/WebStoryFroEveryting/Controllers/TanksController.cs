8d6380d052c4f05314163cfbc9c82e6ac0dff646
Евгений Крепский <evgenkaby89@hotmail.com>
2025-03-21 16:19:53 +0300

    Add something

--- NET19Online/WebStoryFroEveryting/Controllers/TanksController.cs
+++ NET19Online/WebStoryFroEveryting/Controllers/TanksController.cs
@@ -1,7 +1,38 @@
-﻿namespace WebStoryFroEveryting.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStoryFroEveryting.Models.Tanks;
+
+namespace WebStoryFroEveryting.Controllers
 {
-    public class TanksController
+    public class TanksController : Controller
     {
-        string name {  get; set; }
+
+        public IActionResult CreatOrderForTank()
+        {
+            var viewModel = new List<TankViewModel>()
+            {
+                new TankViewModel
+                {
+                    Name = "T-34",
+                    Src = "https://stalin-line.by/images/exposition/Tank_T34/uztm.jpg"
+                },
+                new TankViewModel
+                {
+                    Name = "ИС-2",
+                    Src = "https://parkpatriot.ru/upload/iblock/86b/0A7A5187.JPG"
+                },
+                new TankViewModel
+                {
+                    Name = "Тигр",
+                    Src = "https://tanksdb.ru/images/tanks/tiger.jpg"
+                },
+                new TankViewModel
+                {
+                    Name = "Пантера",
+                    Src = "https://parkpatriot.ru/upload/iblock/30c/Pantera-vo-aremya-rekonstruktsii.jpg"
+                }
+            };
+
+
+            return View(viewModel);
+        }
     }
-}
--- NET19Online/WebStoryFroEveryting/Models/Tanks/TankViewModel.cs
+++ NET19Online/WebStoryFroEveryting/Models/Tanks/TankViewModel.cs
@@ -1,6 +1,9 @@
 ﻿namespace WebStoryFroEveryting.Models.Tanks
+
 {
     public class TankViewModel
     {
+        public string Name { get; set; }
+        public string Src { get; set; }
     }
 }
