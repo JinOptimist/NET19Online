d40289afb3db0ff4827748424568bb720be8c94b
SashaD <daniav1987@dmail.com>
2025-03-30 13:16:20 +0300

    add microservice SwapMeetProduct

---
+++ NET19Online/WebStoryFroEveryting/Controllers/SwapMeetProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStoryFroEveryting.Controllers
+{
+    public class SwapMeetProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ NET19Online/WebStoryFroEveryting/wwwroot/js/SwapMeetProduct/index.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+    const baseUrl = 'https://localhost:7170/';
+    const urlGetProduct = `${baseUrl}getProduct`;
+    const urlAddProduct = `${baseUrl}addProduct`;
+
+    $.get(urlGetProduct)
+        .then(function (products) {
+            products.forEach((product) => {
+                const clone = $('.swapMeetProduct.template').clone();
+                clone.removeClass('template');
+                clone.find('.name').text(product.name);
+                clone.find('.image img').attr('src', product.src);
+                clone.find('.description').text(product.description);
+                clone.find('.price').text(`${product.price}$`);
+                $('.swapMeetProducts').append(clone);
+            });
+        });
+
+    $('.createProduct button').click(function () {
+        const name = $('.nameProduct').val();
+        const src = $('.imageProduct').val();
+        const description = $('.descriptionProduct').val();
+        const price = $('.priceProduct').val();
+
+        const url = `${urlAddProduct}?name=${name}&src=${src}
+                     &description=${description}&price=${price}`;
+        $.get(url);
+    })
+});
+
+
+
