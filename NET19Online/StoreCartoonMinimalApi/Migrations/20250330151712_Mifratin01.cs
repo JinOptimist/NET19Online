ab6eed585900bdb400484a7470b48c603837a36b
Artur <arturdemirchyan21@gmail.com>
2025-04-01 22:11:35 +0300

    CartoonStore

---
+++ NET19Online/StoreCartoonMinimalApi/Migrations/20250330151712_Mifratin01.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoreCartoonMinimalApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Mifratin01 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cartoons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Src = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cartoons", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cartoons");
+        }
+    }
+}
---
+++ NET19Online/WebStoryFroEveryting/wwwroot/js/Cartoon/cartoon.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    const base_usr = 'http://localhost:5032/';
+    const urlGetCartoon = `${base_usr}getCartoons`;
+    const urlAddCartoon = `${base_usr}addCartoons`;
+
+    const point = $.get(urlGetCartoon);
+    point.done(function (data, status, jqXHR) {
+        console.log("Запрос успешен, статус:", status);
+    });
+
+    point.fail(function (jqXHR, status, error) {
+        console.log("Запрос не удался, ошибка:", error);
+    });
+
+    
+
+
+})
