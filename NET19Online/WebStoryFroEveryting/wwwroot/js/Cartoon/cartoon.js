c251ee94fb61e4561611cb1fcb139818c697236a
Artur <arturdemirchyan21@gmail.com>
2025-04-05 21:18:05 +0300

    cartoon

--- NET19Online/WebStoryFroEveryting/wwwroot/js/Cartoon/cartoon.js
+++ NET19Online/WebStoryFroEveryting/wwwroot/js/Cartoon/cartoon.js
@@ -5,14 +5,29 @@
 
     const point = $.get(urlGetCartoon);
     point.done(function (data, status, jqXHR) {
-        console.log("Запрос успешен, статус:", status);
+        console.log("Запрос успешен, статус:", status, data);
     });
-
     point.fail(function (jqXHR, status, error) {
         console.log("Запрос не удался, ошибка:", error);
     });
 
-    
+    $.get(urlGetCartoon)
+        .then(function (data) {
 
+            for (var i = 0; i < data.length; i++) {
+                const item = data[i];
+                const clone = $('.cartoon.block-elements').clone();
+                clone.find('block-elements').removeClass();
+                clone.find('.name').text(item.name);
+                clone.find('.image img').attr('src', item.src);
+                $('.cartoons').append(clone);
+            }
+        });
 
-})
+    $('.add-new-cartoons-container button').click(function () {
+        const name = $('.new-name').val();
+        const src = $('.new-src').val();
+        const url = `${urlAddCartoon}?name=${name}&src=${src}`;
+        $.get(url);
+    })
+});
