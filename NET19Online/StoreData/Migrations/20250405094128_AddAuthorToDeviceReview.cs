573907f001ec913839ec5a0c6ab231ab27f0c4c7
Bnagoc <uladzislau.samueu@gmail.com>
2025-05-03 18:41:23 +0300

    add author to device reviews, a few style changes, add default devices to seed

---
+++ NET19Online/StoreData/Migrations/20250405094128_AddAuthorToDeviceReview.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoreData.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuthorToDeviceReview : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId",
+                table: "GamingDeviceReviews",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GamingDeviceReviews_AuthorId",
+                table: "GamingDeviceReviews",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GamingDeviceReviews_Users_AuthorId",
+                table: "GamingDeviceReviews",
+                column: "AuthorId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GamingDeviceReviews_Users_AuthorId",
+                table: "GamingDeviceReviews");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GamingDeviceReviews_AuthorId",
+                table: "GamingDeviceReviews");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "GamingDeviceReviews");
+        }
+    }
+}
--- NET19Online/StoreData/Seed.cs
+++ NET19Online/StoreData/Seed.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using StoreData.Models;
 using StoreData.Repostiroties;
 
 namespace StoreData
@@ -11,6 +12,7 @@ namespace StoreData
             using var di = serviceProvider.CreateScope();
 
             FillUsers(di.ServiceProvider);
+            FillDevices(di.ServiceProvider);
             FillIdols(di.ServiceProvider);
         }
 
@@ -52,6 +54,66 @@ namespace StoreData
                     Age = 20,
                     Name = "long+hair",
                     Src = "https://oimages.anime-pictures.net/565/5655ec8a7540507d740457a24714ff51.jpg?if=ANIME-PICTURES.NET_-_775334-5334x3000-azur+lane-aegir+%28azur+lane%29-sune+%28mugendai%29-single-long+hair-looking+at+viewer.jpg"
+                });
+            }
+        }
+
+        private void FillDevices(IServiceProvider service)
+        {
+            var deviceRepository = service.GetRequiredService<GamingDeviceRepository>();
+            if (!deviceRepository.Any())
+            {
+                deviceRepository.AddGamingDeviceList(new List<GamingDeviceData>
+                {
+                    new GamingDeviceData
+                    {
+                        Name = "Игровая мышь DeathAdder Essential",
+                        Brand = "Razer",
+                        Price = 93.42m,
+                        Src  = "https://img.5element.by/import/images/ut/goods/good_f3262a0f-ff6a-11eb-bb92-0050560120e8/rz01-03850100-r3m1-razer-igrovaya-mysh-deathadder-essential-1_600.jpg"
+                    },
+                    new GamingDeviceData
+                    {
+                        Name = "Клавиатура Alloy Origins",
+                        Brand = "HyperX",
+                        Price = 359.00m,
+                        Src  = "https://img.5element.by/import/images/ut/goods/good_b4a38539-29f2-11ee-bb94-005056012463/4p4f6aa-aba-hx-kb6rdx-ru-klaviatura-hyperx-alloy-origins-1_600.jpg"
+                    },
+                    new GamingDeviceData
+                    {
+                        Name = "Гарнитура G332",
+                        Brand = "LOGITECH",
+                        Price = 265.00m,
+                        Src  = "https://img.5element.by/import/images/ut/goods/good_4fb9b46b-9678-11e9-80c7-005056840c70/g332-l981-000757-igrovaya-garnitura-logitech-1_600.jpg"
+                    },
+                    new GamingDeviceData
+                    {
+                        Name = "Игровой коврик QcK Hard Pad",
+                        Brand = "STEELSERIES",
+                        Price = 65.40m,
+                        Src  = "https://img.5element.by/import/images/ut/goods/good_b4dbdecc-292e-11e9-80c7-005056840c70/good_img_0aa4b720-30ec-11e9-80c7-005056840c70_600.jpg"
+                    },
+                    new GamingDeviceData
+                    {
+                        Name = "Наушники BlackShark V2 X",
+                        Brand = "Razer",
+                        Price = 245.00m,
+                        Src  = "https://img.5element.by/import/images/ut/goods/good_4f55f6f7-ff6c-11eb-bb92-0050560120e8/rz04-03240100-r3m1-razer-igrovaya-garnitura-blackshark-v2-x-1.jpg"
+                    },
+                    new GamingDeviceData
+                    {
+                        Name = "Клавиатура механическая RK-S98 Blackberry Chartreuse switch",
+                        Brand = "Royal Kludge",
+                        Price = 329.00m,
+                        Src  = "https://img.5element.by/import/images/ut/goods/good_fef397f5-d1b6-11ef-8db4-005056012b6d/rk-s98-blackberry-klaviatura-mehanicheskaya-chartreuse-switch-royal-kludge-1.jpg"
+                    },
+                    new GamingDeviceData
+                    {
+                        Name = "Коврик для мыши Flick XL",
+                        Brand = "Redragon",
+                        Price = 55.90m,
+                        Src  = "https://img.5element.by/import/images/ut/goods/good_8f364e00-5445-11ec-bb94-0050560120e8/77990-igrovoy-kovrik-flick-xl-redragon-1.jpg"
+                    }
                 });
             }
         }
